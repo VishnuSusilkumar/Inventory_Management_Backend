a93ca50524fd282528ebe6e75612cbae0d58b43b
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-12-29 11:23:02 +0530

    feat(app): add cors and morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from "express";
 import { connectDatabase } from "./config/database";
+import cors from "cors";
+import morgan from "morgan";
 import dotenv from "dotenv";
-import inventoryRoutes from './routes/inventory.routes';
+import inventoryRoutes from "./routes/inventory.routes";
 import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/items', inventoryRoutes);
+app.use("/api/items", inventoryRoutes);
 
 connectDatabase();
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
